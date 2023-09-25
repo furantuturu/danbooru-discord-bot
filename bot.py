5eb81893b88e7e55dad6bff19cbf99c2986e2f04
Haemoffy <fuwansuu@gmail.com>
2023-09-25 17:10:02 +0800

    bot.py update v1.02: its now able to accept multiple tags inline filters the image fetched to only receive SFW images

--- bot.py
+++ bot.py
@@ -23,8 +23,8 @@ async def on_ready():
     print(f"{client.user} is now online, ID:{client.user.id}")
 
 @client.command()
-async def tags(ctx, tag_args):
-    tags = tag_args
+async def tags(ctx, *, tag_args):
+    tags = tag_args + " rating:general "
     url = api_posts + urllib.parse.urlencode({"tags": tags, "key": KEY})
     json_data = requests.get(url).json()
     print(url)
@@ -38,10 +38,9 @@ async def tags(ctx, tag_args):
     embed.set_image(url=json_data[i]["file_url"])
     embed.set_author(name=f"ID: {json_data[i]['id']}", url=f"https://danbooru.donmai.us/posts/{json_data[i]['id']}")
     embed.add_field(name="General Tags:", value=json_data[i]["tag_string_general"], inline=False)
-    embed.add_field(name="Character Tags:", value=json_data[i]["tag_string_character"])
-    embed.add_field(name="Game/Anime/Franchise:", value=json_data[i]["tag_string_copyright"])
+    embed.add_field(name="Character Tags:", value=json_data[i]["tag_string_character"], inline=False)
+    embed.add_field(name="Game/Anime/Franchise:", value=json_data[i]["tag_string_copyright"], inline=False)
     embed.add_field(name="Artist:", value=f"{json_data[i]['tag_string_artist']} - https://danbooru.donmai.us/posts?tags={json_data[i]['tag_string_artist']}", inline=False)
-    
     await ctx.send(embed=embed)
     
 def run_discord():
