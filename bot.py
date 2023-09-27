bb6c10961830a9dd4d990b805c37379c5d4ca8d3
Haemoffy <fuwansuu@gmail.com>
2023-09-27 14:07:05 +0800

    bot.py v1.04 update: added replace function to chara tag, origin, and artist fields basically to skip markdown formatting of discord for underscores somehow doesnt happen in general tag field removed the api key since all the bot does is only GET

--- bot.py
+++ bot.py
@@ -3,10 +3,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-KEY = os.getenv("API_KEY")
 
 import discord
-from discord.ext import commands 
+from discord import app_commands
+from discord.ext import commands
+
 import urllib.parse
 import requests
 
@@ -18,8 +19,6 @@ client = commands.Bot(intents=intents, command_prefix=">")
 
 i = 0 # paging purpose
 
-
-
 @client.event
 async def on_ready():
     print(f"{client.user} is now online, ID:{client.user.id}")
@@ -27,11 +26,11 @@ async def on_ready():
 @client.command()
 async def tags(ctx, *, tag_args):
     try:
-        tags = tag_args + " rating:general "
-        url = api_posts + urllib.parse.urlencode({"tags": tags, "key": KEY})
+        tags = tag_args + " rating:general"
+        url = api_posts + urllib.parse.urlencode({"tags": tags})
         json_data = requests.get(url).json()
         print(url)
-        print(json_data[0]["id"])
+        print(json_data[i]["id"])
         
         embed = discord.Embed(
             colour=discord.Color.from_rgb(r=175, g=138, b=101),
@@ -40,16 +39,32 @@ async def tags(ctx, *, tag_args):
         embed.set_thumbnail(url="https://i.ibb.co/ckNDxTD/danbooru-logo-128x128-ea111b6658173e847734.png")
         embed.set_image(url=json_data[i]["file_url"])
         embed.set_author(name=f"ID: {json_data[i]['id']}", url=f"https://danbooru.donmai.us/posts/{json_data[i]['id']}")
-        embed.add_field(name="General Tags:", value=json_data[i]["tag_string_general"], inline=False)
-        embed.add_field(name="Character Tags:", value=json_data[i]["tag_string_character"], inline=False)
-        embed.add_field(name="Game/Anime/Franchise:", value=json_data[i]["tag_string_copyright"], inline=False)
-        embed.add_field(name="Artist:", value=f"{json_data[i]['tag_string_artist']} - https://danbooru.donmai.us/posts?tags={json_data[i]['tag_string_artist']}", inline=False)
+        embed.add_field(
+            name="General Tags:", 
+            value=json_data[i]["tag_string_general"], 
+            inline=False
+            )
+        embed.add_field(
+            name="Character Tags:", 
+            value=json_data[i]["tag_string_character"].replace("_", "\_"), 
+            inline=False)
+        embed.add_field(
+            name="Origin:", 
+            value=json_data[i]["tag_string_copyright"].replace("_", "\_"), 
+            inline=False)
+        embed.add_field(
+            name="Artist:", 
+            value=f'{json_data[i]["tag_string_artist"].replace("_", chr(92) + "_")} - https://danbooru.donmai.us/posts?tags={json_data[i]["tag_string_artist"]}', # chr(92) is "\"", bc f-strings doesnt allow "\"
+            inline=False)
+        
         await ctx.send(embed=embed)
+        
     except KeyError:
         err_embed = discord.Embed(
             colour=discord.Color.from_rgb(r=175, g=138, b=101),
             description="⚠️ You cannot search for more than 2 tags at a time."
         )
+        
         await ctx.send(embed=err_embed)
         
     
