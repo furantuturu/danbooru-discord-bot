e8bd7f0b915c10b561a85538a4709e7ec1fda9a8
Haemoffy <fuwansuu@gmail.com>
2023-09-29 14:14:21 +0800

    bot.py v1.07: relocated all embed codes to embeds.py a purpose file to store all embeds implemnted the code of the fetchbooru regular command to it's slash command version the rest are just code polishing

--- bot.py
+++ bot.py
@@ -11,13 +11,23 @@ from discord.ext import commands
 import urllib.parse
 import requests
 
+import embeds
+
 api_posts = "https://danbooru.donmai.us/posts.json?"
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = commands.Bot(intents=intents, command_prefix=">")
 
-i = 0 # paging purpose
+# color(s)
+embed_color = discord.Color.from_rgb(r=175, g=138, b=101)
+
+# error msg
+key_err_msg = "⚠️ I don't own a premium account. You cannot search for more than 2 tags at a time."
+ind_err_msg = "⚠️ Either it contains explicit content or it doesn't exist."
+
+# paging purpose
+i = 0 
 
 @client.event
 async def on_ready():
@@ -30,62 +40,63 @@ async def on_ready():
         print(Exception)
 
 @client.command()
-async def fetchbooru(ctx, *, tag_args: str):
+async def fetchbooru(ctx, *, tags: str):
     try:
-        tags = tag_args + " rating:general"
-        url = api_posts + urllib.parse.urlencode({"tags": tags})
+        tag_args = tags + " rating:general"
+        url = api_posts + urllib.parse.urlencode({"tags": tag_args})
         json_data = requests.get(url).json()
         print(url)
         print(json_data[i]["id"])
         
-        embed = discord.Embed(
-            colour=discord.Color.from_rgb(r=175, g=138, b=101),
-            title="Search results:"
-        )
-        embed.set_thumbnail(url="https://i.ibb.co/ckNDxTD/danbooru-logo-128x128-ea111b6658173e847734.png")
-        embed.set_image(url=json_data[i]["file_url"])
-        embed.set_author(name=f"ID: {json_data[i]['id']}", url=f"https://danbooru.donmai.us/posts/{json_data[i]['id']}")
-        embed.add_field(
-            name="General Tags:", 
-            value=json_data[i]["tag_string_general"], 
-            inline=False
-            )
-        embed.add_field(
-            name="Character Tags:", 
-            value=json_data[i]["tag_string_character"].replace("_", "\_"), 
-            inline=False)
-        embed.add_field(
-            name="Origin:", 
-            value=json_data[i]["tag_string_copyright"].replace("_", "\_"), 
-            inline=False)
-        embed.add_field(
-            name="Artist:", 
-            value=f'{json_data[i]["tag_string_artist"].replace("_", chr(92) + "_")} - https://danbooru.donmai.us/posts?tags={json_data[i]["tag_string_artist"]}', # chr(92) is "\"", bc f-strings doesnt allow "\"
-            inline=False)
+        embed = embeds.main_response_embed(json_data=json_data, page=i, embed_color=embed_color)
         
         await ctx.send(embed=embed)
         
     except KeyError:
         err_embed = discord.Embed(
-            colour=discord.Color.from_rgb(r=175, g=138, b=101),
-            description="⚠️ You cannot search for more than 2 tags at a time."
-        )
-        
-        await ctx.send(embed=err_embed)
-    
-    except IndexError:
-        err_embed = discord.Embed(
-            colour=discord.Color.from_rgb(r=175, g=138, b=101),
-            description="⚠️ Either it contains explicit content or it doesn't exist."
+            colour=embed_color,
+            description=key_err_msg
         )
         
         await ctx.send(embed=err_embed)
 
+    except IndexError:
+        err_embed = discord.Embed(
+            colour=embed_color,
+            description=ind_err_msg
+        )
+        
+        await ctx.send(embed=err_embed)
+        
 @client.tree.command(name="fetchbooru", description="Fetch an art/image with the 2 tags of your choice")
-@app_commands.describe(tag1="Enter a danbooru valid tag")        
-@app_commands.describe(tag2="Enter a danbooru valid tag")        
-async def fetchbooru(interaction: discord.Interaction, tag1: str, tag2: str):
-    await interaction.response.send_message(f"Your tags: {tag1}, {tag2} ")
+@app_commands.describe(tags="Enter a danbooru valid format tags, separated by space")        
+async def fetchbooru(interaction: discord.Interaction, tags: str):
+    try:
+        tag_args = tags + " rating:general"
+        url = api_posts + urllib.parse.urlencode({"tags": tag_args})
+        json_data = requests.get(url).json()
+        print(url)
+        print(json_data[i]["id"])
+        
+        embed = embeds.main_response_embed(json_data=json_data, page=i, embed_color=embed_color)
+        
+        await interaction.response.send_message(embed=embed)
+        
+    except KeyError:
+        err_embed = discord.Embed(
+            colour=embed_color,
+            description=key_err_msg
+        )
+        
+        await interaction.response.send_message(embed=err_embed)
+
+    except IndexError:
+        err_embed = discord.Embed(
+            colour=embed_color,
+            description=ind_err_msg
+        )
+        
+        await interaction.response.send_message(embed=err_embed)
 
 def run_discord():
     client.run(DISCORD_TOKEN)
